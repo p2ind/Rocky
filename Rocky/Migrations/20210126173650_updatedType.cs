47d153fe16e4cde3e2ba89f890f90332643fc9ec
Pankaj Saxena <saxena.pankaj7055@gmail.com>
2021-01-28 05:49:47 +0530

    Add project files.

---
+++ Rocky/Migrations/20210126173650_updatedType.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rocky.Migrations
+{
+    public partial class updatedType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Type",
+                table: "Type");
+
+            migrationBuilder.RenameTable(
+                name: "Type",
+                newName: "ApplicationType");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ApplicationType",
+                table: "ApplicationType",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ApplicationType",
+                table: "ApplicationType");
+
+            migrationBuilder.RenameTable(
+                name: "ApplicationType",
+                newName: "Type");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Type",
+                table: "Type",
+                column: "Id");
+        }
+    }
+}
